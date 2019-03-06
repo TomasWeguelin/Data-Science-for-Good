3d54ce21ff39620474c6083e576ec0a9accba9bf
Gabrielb22b <gabriel_fds5@hotmail.com>
2019-03-06 11:11:50 -0300

    Não sei reverter
    estou tentando reverter mas nao consigo

--- Data_Science_For_Good.R
+++ Data_Science_For_Good.R
@@ -4,9 +4,3 @@ for(i in 1:length(packages)){
     install.packages(packages[i])
   library(packages[i],character.only=TRUE)
 }
-<<<<<<< HEAD
-
-
-#### CHupa gabriel ####
-=======
->>>>>>> parent of e75574d... Modification
